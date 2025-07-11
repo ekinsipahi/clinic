0149aabaf09eca484270823dce94ae705fb1a854
Ekin Sipahi <ekinsipahi8@gmail.com>
2025-07-11 20:40:47 +0300

    Kalıcı database eklendi, SUPABASE

--- devrim_website/settings.py
+++ devrim_website/settings.py
@@ -86,11 +86,16 @@ WSGI_APPLICATION = "devrim_website.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.pepjtuncgggbcwacefdg',
+        'PASSWORD': '27J43*12_!Z12a',
+        'HOST': 'aws-0-eu-north-1.pooler.supabase.com',
+        'PORT': '5432',  # DOĞRUDAN bağlantı
     }
 }
+
 
 
 # Password validation
