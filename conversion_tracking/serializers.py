529d8fc382b50e80590431d4b3b3457874000a77
Ekin Sipahi <ekinsipahi8@gmail.com>
2025-07-11 20:44:57 +0300

    aynı gclid iki kere post sorunu çözüldü

--- conversion_tracking/serializers.py
+++ conversion_tracking/serializers.py
@@ -8,5 +8,9 @@ class ConversionSerializer(serializers.ModelSerializer):
         fields = ['gclid', 'conversion_name', 'page']
 
     def create(self, validated_data):
+        gclid = validated_data.get('gclid')
+        if gclid and Conversion.objects.filter(gclid=gclid).exists():
+            raise serializers.ValidationError({'gclid': 'This GCLID has already been recorded.'})
+        
         validated_data['conversion_time'] = datetime.utcnow()  # Google Ads UTC ister
         return super().create(validated_data)
